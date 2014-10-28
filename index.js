ef74ad4e76897a24f30399c3dea41d0df255c880
Hareendra Seneviratne <hareendras@gmail.com>
2014-10-28 06:20:21 +0000

    inde.js modified

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
+var fs = require('fs');
 var app = express();
+var buff = new Buffer();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World22!')
+  response.send(buff.toString(fs.readFile('index.html')));
 })
 
 app.listen(app.get('port'), function() {
